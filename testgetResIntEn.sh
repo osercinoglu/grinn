daffe479b3e84f5deb28193b5af8ea1fb901f5f5
Onur Sercinoglu <onursercin@gmail.com>
2017-10-25 14:09:29 +0300

    Code reorg
    Former-commit-id: ceaa70ca9063909d829b617bd6f55c23dcfaf4f3

--- testgetResIntEn.sh
+++
@@ -1,3 +0,0 @@
-rm log.log;
-rm outFolder -R;
-./getResIntEn.py --pdb test.pdb --psf test.psf --dcd test.dcd --numcores 8 --targetsel resid 0 to 10 --sourcesel resid 0 to 10 --paircalc --pairfiltercutoff 12 --pairfilterpercentage 10 --skip 100 --namd2exe NAMD_2.12b1/namd2 --outfolder outFolder --logfile log.log --parameterfile False --resintcorr --topickle
