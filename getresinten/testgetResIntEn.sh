3b6da64becb32d30883478a9f48b6a4cd9a3d01d
Onur Serçinoğlu <onursercin@gmail.com>
2017-12-11 23:15:09 +0300

    Initial renaming and reorganizing.
    Former-commit-id: 1af40dfaa7b7f330dc638871e94998f5b32a93bf

--- getresinten/testgetResIntEn.sh
+++
@@ -1,3 +0,0 @@
-rm log.log;
-rm -R outFolder ;
-./getResIntEn.py --pdb ../test/test.pdb --psf ../test/test.psf --dcd ../test/test.dcd --numcores 3 --targetsel resindex 0 to 3 --sourcesel resindex 0 to 3 --paircalc --pairfiltercutoff 12 --pairfilterpercentage 10 --skip 250 --namd2exe ../NAMD_2.12_MacOSX-x86_64-multicore/namd2 --outfolder outFolder --logfile log.log --resintcorr --topickle
