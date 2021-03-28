da5a73e5e0da64a630939a0cf7bc97667fdf8ccf
onursercinoglu <onursercin@gmail.com>
2021-03-29 01:10:49 +0300

    Cancelled wait() calls, switching from map_async() to map().

--- source/calc.py
+++ source/calc.py
@@ -492,15 +492,15 @@ def calcEnergiesNAMD(params):
 	params.logger = None
 
 	# Cancelling the following for map_async, it was intended for python 2.7
-	results = pool.map_async(calcEnergiesSingleCoreNAMD,
-		zip(params.pairsFilteredChunks,itertools.repeat(params))).get(9999999)
-	results.wait()
-	print('I got past map_async call.')
+	#results = pool.map_async(calcEnergiesSingleCoreNAMD,
+	#	zip(params.pairsFilteredChunks,itertools.repeat(params))).get(99999)
+	
 
 	# Instead, the following line.
-	#results = pool.map(calcEnergiesSingleCoreNAMD,
-	#	zip(params.pairsFilteredChunks,itertools.repeat(params)))
+	results = pool.map(calcEnergiesSingleCoreNAMD,
+		zip(params.pairsFilteredChunks,itertools.repeat(params)))
 
+	print('I got past map_async call.')
 	pool.close()
 	pool.join()
 	
@@ -537,17 +537,16 @@ def calcEnergiesNAMD(params):
 	pool = multiprocessing.Pool(params.numCores)
 
 	# Cancelling the following for map_async, it was intended for python 2.7
-	parsedEnergiesResults = pool.map_async(parseEnergiesSingleCoreNAMD,
-		zip(energiesFilePathsChunks,itertools.repeat(os.path.join(
-			params.outFolder,'system.pdb')),
-			itertools.repeat(params.logFile))).get(9999999)
-	results.wait()
-
-	# Instead, the following line.
-	#parsedEnergiesResults = pool.map(parseEnergiesSingleCoreNAMD,
+	#parsedEnergiesResults = pool.map_async(parseEnergiesSingleCoreNAMD,
 	#	zip(energiesFilePathsChunks,itertools.repeat(os.path.join(
 	#		params.outFolder,'system.pdb')),
-	#		itertools.repeat(params.logFile)))
+	#		itertools.repeat(params.logFile))).get(9999999)
+	
+	# Instead, the following line.
+	parsedEnergiesResults = pool.map(parseEnergiesSingleCoreNAMD,
+		zip(energiesFilePathsChunks,itertools.repeat(os.path.join(
+			params.outFolder,'system.pdb')),
+			itertools.repeat(params.logFile)))
 
 	parsedEnergies = dict()
 	for parsedEnergiesResult in parsedEnergiesResults:
